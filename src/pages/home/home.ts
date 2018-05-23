965101e9ea8b676b97fcedc9cf4cf9307ede261d
Phakpoom Ittirattanakomon <beerbc16@gmail.com>
2018-05-23 11:36:16 +0700

    Edit SignIn method

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,16 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  msg = "";
+  username;
+  password;
   constructor(public navCtrl: NavController) {
 
   }
 
-  signIn()
+  signIn(username,password)
   {
-    alert("Signed in jaaaaa");
+    // alert("OK " + username + " " + password);
+    // let msg;
+    if(username==="admin"&&password==="superman")
+    {
+      this.msg = "OK";
+    }
+    else
+    {
+      this.msg = "Invalid username or password";
+    }
+    // alert(msg);
   }
-
+  clearForm()
+  {
+    this.username="";
+    this.password="";
+  }
 
 
 }
